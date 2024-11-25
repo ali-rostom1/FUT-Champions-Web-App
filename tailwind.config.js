bfbac282f9e511988dfe7e84fd271d16482abd1f
ali-rostom1 <ali.rostom220@gmail.com>
2024-11-25 20:28:32 +0100

    #5,#3 dev : configure tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,2 +1,12 @@
-module.exports = {};
-
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          'darkGreen':'#029356',
+          'teal':'#009eb0',
+          'blue':'#0073e6',
+          'whitesSmoke':'#f5f5f5',
+        }
+      },
+    },  
+  }
