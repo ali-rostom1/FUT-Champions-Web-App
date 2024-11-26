221dbfb131b1b3d3eaf99ff3ec454649752bf539
ali-rostom1 <ali.rostom220@gmail.com>
2024-11-26 14:31:13 +0100

    #7 front end : add player object to manipulate Player data from form

--- src/js/script.js
+++ src/js/script.js
@@ -12,4 +12,17 @@ closeModalBtn.addEventListener('click', function(){
 openModalBtn.addEventListener('click', function(){
     myModal.classList.toggle('invisible');
     document.body.classList.toggle('overflow-hidden');
-})
+});
+
+function Player(name,pos,phy,def,sho){
+    this.name = name;
+    this.pos = pos;
+    this.phy = phy;
+    this.def = def;
+    this.sho = sho;
+    this.createCard = function(){
+        let div = document.createElement('div');
+        return div;
+    };
+}
+
