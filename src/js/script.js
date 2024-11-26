99883fc4c897927fe071c136050acf3a7c713d01
ali-rostom1 <ali.rostom220@gmail.com>
2024-11-26 14:20:43 +0100

    #7 front end : add form modal popup interaction

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,15 @@
+
+
 mobileMenuToggle.addEventListener('click', function (){
     mobileMenu.classList.toggle('hidden');
 });
 
-
+closeModalBtn.addEventListener('click', function(){
+    myModal.classList.toggle('invisible');
+    document.body.classList.toggle('overflow-hidden');
+    addForm.reset();
+});
+openModalBtn.addEventListener('click', function(){
+    myModal.classList.toggle('invisible');
+    document.body.classList.toggle('overflow-hidden');
+})
