2e7279025062f315fa71811d64878b8568be07fc
ali-rostom1 <ali.rostom220@gmail.com>
2024-11-26 17:09:19 +0100

    #8 front end : add dynamic form animation feature

--- src/js/script.js
+++ src/js/script.js
@@ -7,20 +7,12 @@ mobileMenuToggle.addEventListener('click', function (){
 closeModalBtn.addEventListener('click', function(){
     myModal.classList.toggle('invisible');
     document.body.classList.toggle('overflow-hidden');
-    addForm.reset();
+    resetForm(addForm);
 });
 openModalBtn.addEventListener('click', function(){
     myModal.classList.toggle('invisible');
     document.body.classList.toggle('overflow-hidden');
-    nameInput.addEventListener('input', function(){
-        if(nameInput.value != "" && nameInput.value.length > 5 && noNumberString(nameInput.value)){
-            nameInput.classList.add('border-green-600','border-2');
-            nameInput.classList.remove('border-red-600');
-        }else{
-            nameInput.classList.add('border-red-600','border-2');
-            nameInput.classList.remove('border-green-600');
-        }
-    })
+    dynamicNameFormValidation(nameInput);
 });
 
 function Player(name,pos,phy,def,sho){
@@ -34,11 +26,22 @@ function Player(name,pos,phy,def,sho){
         return div;
     };
 }
+
 Players = [];
 console.log(Players);
+
 addForm.addEventListener('submit',function(event){
     event.preventDefault();
-    
+    if(nameInput.classList.contains('border-green-600')){
+    }else{
+        console.log(nameInput.nextSibling);
+        if(!nameInput.nextElementSibling){
+            let div = document.createElement('div');
+            div.classList = 'text-red-600';
+            div.innerText = 'ERROR'
+            nameInput.after(div);
+        }
+    }
     let pos = document.querySelector('input[name="Position"]:checked').value;
     let phy = document.getElementById('PHY').value;
     let def = document.getElementById('DEF').value;
@@ -59,4 +62,28 @@ function noNumberString(string){
         }
     }
     return 1;
+}
+
+
+function resetForm(form){
+    nameInput.classList = 'w-full px-4 py-2 border border-gray-300 rounded-md'; //name input reset
+    if(nameInput.nextElementSibling) nameInput.nextElementSibling.remove();
+    form.reset();
+}
+
+
+function dynamicNameFormValidation(input){
+    input.addEventListener('input', function(){
+        if(input.value != "" && input.value.length > 5 && noNumberString(input.value)){
+            input.classList.add('border-green-600','border-2','outline-none');
+            input.classList.remove('border-red-600');
+            if(input.nextElementSibling) input.nextElementSibling.remove();
+        }else if(input.value == ""){
+            input.classList.remove('border-green-600','border-red-600','outline-none');
+        }
+        else{
+            input.classList.add('border-red-600','border-2','outline-none');
+            input.classList.remove('border-green-600');
+        }
+    });
 }
